072a56835a3300a4205853ef02977ce8d9764044
hyy <hyy@hyy.hyy>
2022-04-11 23:59:07 +0800

    init for emacs basic setting

---
+++ README.md
@@ -0,0 +1,1 @@
+git clone https://github.com/hyyfrank/emacsconfig.git ~/.emacs.d
--- init.el
+++ init.el
@@ -1,9 +1,9 @@
 
-(require 'package)
-
-
-
-(setq package-enable-at-startup nil)
+;;; init.el --- <custom my emacs for c plus plus env>
+;;; Commentary:
+;;; Code:
+(package-initialize)
+(setq inhibit-startup-message t)
 
 (add-to-list 'package-archives
 
@@ -14,6 +14,7 @@
   	     '("gnu" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/"))
 
 (package-initialize)
+(require 'package)
 
 
 
@@ -27,35 +28,24 @@
 
 
 
-(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))  ;;从这里加载
+(org-babel-load-file (expand-file-name "~/.emacs.d/setting.org"))  ;;从这里加载
+
+;; (org-babel-load-file (expand-file-name "~/.emacs.d/orgconf.org"))  ;;从这里加载
+
 
 (custom-set-variables
-
  ;; custom-set-variables was added by Custom.
-
  ;; If you edit it by hand, you could mess it up, so be careful.
-
  ;; Your init file should contain only one such instance.
-
  ;; If there is more than one, they won't work right.
-
  '(package-selected-packages
-
-   (quote
-
-    (xcscope 0x0 0blayout posframe zenburn-theme which-key use-package try solarized-theme org-bullets counsel auto-complete ace-window))))
+   '(eglot lsp-mode forge magit counsel-projectile rainbow-delimiters dumb-jump ivy-prescient doom-themes ivy-rich doom-modeline all-the-icons xcscope 0x0 0blayout posframe zenburn-theme which-key use-package try solarized-theme org-bullets counsel auto-complete ace-window)))
 
 (custom-set-faces
-
  ;; custom-set-faces was added by Custom.
-
  ;; If you edit it by hand, you could mess it up, so be careful.
-
  ;; Your init file should contain only one such instance.
-
  ;; If there is more than one, they won't work right.
-
  )
-
-
+;;; init.el ends here
 
